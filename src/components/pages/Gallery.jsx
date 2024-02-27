7736444edcbaa8755653bcefc18cbdeef7a4ff22
Kritta <142368906+Krittanai2933@users.noreply.github.com>
2024-02-27 15:03:28 +0700

    Update Gallery.jsx

--- src/components/pages/Gallery.jsx
+++ src/components/pages/Gallery.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import '../Gallery.css'
-import gal1 from '/Users/dm/Desktop/mainmai/TBC2024_Website/src/assets/gallery/gal1.png';
-import gal2 from '/Users/dm/Desktop/mainmai/TBC2024_Website/src/assets/gallery/gal2.png';
-import gal3 from '/Users/dm/Desktop/mainmai/TBC2024_Website/src/assets/gallery/gal3.png';
-import gal4 from '/Users/dm/Desktop/mainmai/TBC2024_Website/src/assets/gallery/gal4.png';
-import gal5 from '/Users/dm/Desktop/mainmai/TBC2024_Website/src/assets/gallery/gal5.png';
-import gal6 from '/Users/dm/Desktop/mainmai/TBC2024_Website/src/assets/gallery/gal6.png';
-import gal7 from '/Users/dm/Desktop/mainmai/TBC2024_Website/src/assets/gallery/gal7.png';
-import gal8 from '/Users/dm/Desktop/mainmai/TBC2024_Website/src/assets/gallery/gal8.png';
-import gal9 from '/Users/dm/Desktop/mainmai/TBC2024_Website/src/assets/gallery/gal9.png';
+import gal1 from '../..//assets/gallery/gal1.png';
+import gal2 from '../../assets/gallery/gal2.png';
+import gal3 from '../../assets/gal3.png';
+import gal4 from '../../assets/gallery/gal4.png';
+import gal5 from '../../assets/gallery/gal5.png';
+import gal6 from '../../assets/gallery/gal6.png';
+import gal7 from '../../assets/gallery/gal7.png';
+import gal8 from '../../assets/gallery/gal8.png';
+import gal9 from '../../assets/gallery/gal9.png';
 
 
 
