308384cf80d581613f515e892add75cdb988655f
Kritta <142368906+Krittanai2933@users.noreply.github.com>
2024-04-11 19:52:41 +0700

    Revert "Speakers"

--- src/components/pages/Carousel2.jsx
+++ src/components/pages/Carousel2.jsx
@@ -2,38 +2,26 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import image1 from '../../assets/speakers/image1.jpeg';
-import image2 from '../../assets/speakers/image2.png';
-import image3 from '../../assets/speakers/image3.png';
-import image4 from '../../assets/speakers/image4.jpg';
-import image5 from '../../assets/speakers/image5.jpg';
-import image6 from '../../assets/speakers/image6.png';
-import image7 from '../../assets/speakers/image7.png';
-import image8 from '../../assets/speakers/image8.png';
-import image9 from '../../assets/speakers/image9.jpeg';
-import image10 from '../../assets/speakers/image10.png';
-import image11 from '../../assets/speakers/image11.jpg';
-import image12 from '../../assets/speakers/image12.png';
-import image13 from '../../assets/speakers/image13.png';
-import image14 from '../../assets/speakers/image14.jpg';
-import image15 from '../../assets/speakers/image15.jpeg';
-import image16 from '../../assets/speakers/image16.png';
-import image17 from '../../assets/speakers/image17.jpg';
-import image18 from '../../assets/speakers/image18.jpg';
-import image19 from '../../assets/speakers/image19.png';
-import image20 from '../../assets/speakers/image20.jpg';
-import image21 from '../../assets/speakers/image21.jpg';
-import image22 from '../../assets/speakers/image22.png';
-import image23 from '../../assets/speakers/image23.png';
-import image24 from '../../assets/speakers/image24.jpg';
-import image25 from '../../assets/speakers/image25.jpg';
-import image26 from '../../assets/speakers/image26.png';
-import image27 from '../../assets/speakers/image27.jpg';
-import image28 from '../../assets/speakers/image28.jpg';
-import image29 from '../../assets/speakers/image29.gif';
-import image30 from '../../assets/speakers/image30.jpg';
-import image31 from '../../assets/speakers/image30.jpg';
-
+import image1 from '../../assets/speakers/1.png';
+import image2 from '../../assets/speakers/2.png';
+import image3 from '../../assets/speakers/3.png';
+import image4 from '../../assets/speakers/4.png';
+import image5 from '../../assets/speakers/5.png';
+import image6 from '../../assets/speakers/6.png';
+import image7 from '../../assets/speakers/7.png';
+import image8 from '../../assets/speakers/8.png';
+import image9 from '../../assets/speakers/9.png';
+import image10 from '../../assets/speakers/10.png';
+import image11 from '../../assets/speakers/11.png';
+import image12 from '../../assets/speakers/12.png';
+import image13 from '../../assets/speakers/13.png';
+import image14 from '../../assets/speakers/14.png';
+import image15 from '../../assets/speakers/15.png';
+import image16 from '../../assets/speakers/16.png';
+import image17 from '../../assets/speakers/17.png';
+import image18 from '../../assets/speakers/18.png';
+import image19 from '../../assets/speakers/19.png';
+import image20 from '../../assets/speakers/20.png';
 import shadow from '../../assets/balck_shadow.png';
 
 import '../Carousel2.css';
@@ -48,39 +36,28 @@ export const Carousel2 = () => {
     };
 
     const speakers = [
-        { image: image1, name: 'Watcharaporn Donsang (Aom)' },
-        { image: image2, name: 'Tip_NZ' },
-        { image: image3, name: 'Paco De La India' },
-        { image: image4, name: 'เพียว' },
-        { image: image5, name: 'Jimmy Kostro' },
-        { image: image6, name: 'Dr. Napapatch Piyachaiyakul' },
-        { image: image7, name: 'Sakchai Pratantip' },
-        { image: image8, name: 'Pisut Oncharoen' },
-        { image: image9, name: 'Koji Higashi' },
-        { image: image10, name: 'Albert Buu' },
-        { image: image11, name: 'Dea Rezkhita' },
-        { image: image12, name: 'Michael R. Keag' },
-        { image: image13, name: 'Rolan Bewick' },
-        { image: image14, name: 'Piccolo' },
-        { image: image15, name: 'Deja Piyavhatkul' },
-        { image: image16, name: 'Chayoot Anukoolkarn' },
-        { image: image17, name: 'Siraphop Nilbodee' },
-        { image: image18, name: 'Thanakrit Sangnongban' },
-        { image: image19, name: 'Wichit Saiklao' },
-        { image: image20, name: 'Piriya Sambandaraksa' },
-        { image: image21, name: 'Captain Sidd' },
-        { image: image22, name: 'Suppakrit Boonsat' },
-        { image: image23, name: 'Pitcha Suthigul (นายพิชชา สุทธิกุล)' },
-        { image: image24, name: 'Wit Sittivaekin' },
-        { image: image25, name: 'Kgothatso Ngako' },
-        { image: image26, name: 'Piyawan Leelarungrayub' },
-        { image: image27, name: 'Gracialo' },
-        { image: image28, name: 'Kittapob Wachatimanont' },
-        { image: image29, name: 'Paksuda pattarawanitch' },
-        { image: image30, name: 'Jakk Goodday' },
-        { image: image31, name: 'Gu Guy' }
-    ];
-    
+      { image: image1, name: 'Albert Buu'},
+      // { image: image2, name: 'Speaker 2' },
+      { image: image3, name: 'Deja Piyavhatkul'},
+      { image: image4, name: 'Chayoot Anukoolkarn' },
+      { image: image5, name: 'Dea Rezkhita'},
+      { image: image6, name: 'Captain Sidd' },
+      { image: image7, name: 'เพียว Cat Money'},
+      { image: image8, name: 'Piccolo' },
+      { image: image9, name: 'Pisut Oncharoen'},
+      { image: image10, name: 'Piriya Sambandaraksa'},
+      { image: image11, name: 'Dr.Big'},
+      { image: image12, name: 'Jimmy Kostro'},
+      { image: image13, name: 'Michael R. Keag'},
+      { image: image14, name: 'Rolan Bewick'},
+      { image: image15, name: 'Thanakrit Sangnongban' },
+      // { image: image16, name: 'Speaker 16'},
+      { image: image17, name: 'Wichit Saiklao'},
+      { image: image18, name: 'Siraphop Nilbodee' },
+      { image: image19, name: 'Sakchai Pratantip'},
+      { image: image20, name: 'Koji Higashi'}
+      
+  ];
 
   return (
       <div className="container">
