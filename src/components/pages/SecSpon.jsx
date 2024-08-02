03b2d7d05e060c1b76147233c7b7bcc65086e33e
Kritta <142368906+Krittanai2933@users.noreply.github.com>
2024-08-02 18:48:41 +0900

    Update SecSpon.jsx

--- src/components/pages/SecSpon.jsx
+++ src/components/pages/SecSpon.jsx
@@ -1,53 +1,97 @@
 import React from "react";
 import "../Secspon.css";
+import neutronpay from '../../assets/sponsor/logos/logo_square/neutronpay.png';
+import mebit from '../src/assets/sponsor/logos/logo_square/mebit.png';
+import okx from '../src/assets/sponsor/logos/logo_square/okx.png';
+import beer_pot from '../src/assets/sponsor/logos/logo_square/beer_pot.png';
+import toffeecake from '../src/assets/sponsor/logos/logo_square/toffeecake.png';
+import bob from '../src/assets/sponsor/logos/logo_square/bob.png';
+import chitbeer from '../src/assets/sponsor/logos/logo_square/chitbeer.png';
+import bitcast from '../src/assets/sponsor/logos/logo_square/bitcast.png';
+import lates from '../src/assets/sponsor/logos/logo_square/lates.png';
+import trued from '../src/assets/sponsor/logos/logo_square/true.png';
+import crossvibe from '../src/assets/sponsor/logos/logo_square/crossvibe.png';
+import lumen from '../src/assets/sponsor/logos/logo_square/lumen.png';
+import newyorkstu from '../src/assets/sponsor/logos/logo_square/newyorkstu.png';
+import codekids from '../src/assets/sponsor/logos/logo_square/codekids.png';
+import seed from '../src/assets/sponsor/logos/logo_square/se_ed.png';
+import digdig from '../src/assets/sponsor/logos/logo_square/digdig.png';
+import oasis_coffee from '../src/assets/sponsor/logos/logo_square/oasis_coffee.png';
+import bitcoin_learning from '../src/assets/sponsor/logos/logo_square/bitcoin_learning.png';
+import sbp from '../src/assets/sponsor/logos/logo_square/sbp.png';
+import siambc from '../src/assets/sponsor/logos/logo_square/siambc.png';
+import shopbit from '../src/assets/sponsor/logos/logo_square/shopbit.png';
+import radars_point from '../src/assets/sponsor/logos/logo_square/radars_point.png';
+import bitcoin_vn from '../src/assets/sponsor/logos/logo_square/bitcoin_vn.png';
+import cdc from '../src/assets/sponsor/logos/logo_square/cdc.png';
+import crispywill from '../src/assets/sponsor/logos/logo_square/crispywill.png';
+import lunchblock from '../src/assets/sponsor/logos/logo_square/lunchblock.png';
+import iwear from '../src/assets/sponsor/logos/logo_square/iwear.png';
+import wherosti from '../src/assets/sponsor/logos/logo_square/wherosti.png';
+import bitcoin_legacy from '../src/assets/sponsor/logos/logo_square/bitcoin_legacy.png';
+import thai_lightning from '../src/assets/sponsor/logos/logo_square/thai_lightning.png';
+import catdumb from '../src/assets/sponsor/logos/logo_square/catdumb.png';
+import spotlight from '../src/assets/sponsor/logos/logo_square/spotlight.png';
+import siam_blockchain from '../src/assets/sponsor/logos/logo_square/siam_blockchain.png';
+import mgr_online from '../src/assets/sponsor/logos/logo_square/mgr_online.png';
+import i_bit from '../src/assets/sponsor/logos/logo_square/i_bit.png';
+import wealthme_up from '../src/assets/sponsor/logos/logo_square/wealthme_up.png';
+import longtunman from '../src/assets/sponsor/logos/logo_square/longtunman.png';
+import trader_kp from '../src/assets/sponsor/logos/logo_square/trader_kp.png';
+import cat_money from '../src/assets/sponsor/logos/logo_square/cat_money.png';
+import business_tmr from '../src/assets/sponsor/logos/logo_square/business_tmr.png';
+import thairath from '../src/assets/sponsor/logos/logo_square/thairath.png';
+import ploy from '../src/assets/sponsor/logos/logo_square/ploy.png';
+import lady_crypto from '../src/assets/sponsor/logos/logo_square/lady_crypto.png';
+import bitcoin_addict from '../src/assets/sponsor/logos/logo_square/bitcoin_addict.png';
+import efinance from '../src/assets/sponsor/logos/logo_square/efinance.png';
 
 const logos = [
-  { url: "../../assets/sponsor/logos/logo_square/neutronpay.png", alt: "Neutronpay" },
-  { url: "../src/assets/sponsor/logos/logo_square/mebit.png", alt: "Mebit" },
-  { url: "../src/assets/sponsor/logos/logo_square/okx.png", alt: "OKX" },
-  { url: "../src/assets/sponsor/logos/logo_square/beer_pot.png", alt: "Beer_Pot" },
-  { url: "../src/assets/sponsor/logos/logo_square/toffeecake.png", alt: "Toffee Cake" },
-  { url: "../src/assets/sponsor/logos/logo_square/bob.png", alt: "BOB" },
-  { url: "../src/assets/sponsor/logos/logo_square/chitbeer.png", alt: "Chitbeer" },
-  { url: "../src/assets/sponsor/logos/logo_square/bitcast.png", alt: "Bitcast" },
-  { url: "../src/assets/sponsor/logos/logo_square/lates.png", alt: "Lates" },
-  { url: "../src/assets/sponsor/logos/logo_square/true.png", alt: "True Park" },
-  { url: "../src/assets/sponsor/logos/logo_square/crossvibe.png", alt: "Cross Vibe Hotel" },
-  { url: "../src/assets/sponsor/logos/logo_square/lumen.png", alt: "Lumen Bangkok Udomsuk Hotel" },
-  { url: "../src/assets/sponsor/logos/logo_square/newyorkstu.png", alt: "New York Stu" },
-  { url: "../src/assets/sponsor/logos/logo_square/codekids.png", alt: "Code_Kids" },
-  { url: "../src/assets/sponsor/logos/logo_square/se_ed.png", alt: "SE_ED" },
-  { url: "../src/assets/sponsor/logos/logo_square/digdig.png", alt: "DigDig" },
-  { url: "../src/assets/sponsor/logos/logo_square/oasis_coffee.png", alt: "Oasis_Coffee" },
-  { url: "../src/assets/sponsor/logos/logo_square/bitcoin_learning.png", alt: "Bitcoin_Learning" },
-  { url: "../src/assets/sponsor/logos/logo_square/sbp.png", alt: "SBP" },
-  { url: "../src/assets/sponsor/logos/logo_square/siambc.png", alt: "SiamBC" },
-  { url: "../src/assets/sponsor/logos/logo_square/shopbit.png", alt: "Shopbit" },
-  { url: "../src/assets/sponsor/logos/logo_square/radars_point.png", alt: "Radars_Point" },
-  { url: "../src/assets/sponsor/logos/logo_square/bitcoin_vn.png", alt: "Bitcoin_VN" },
-  { url: "../src/assets/sponsor/logos/logo_square/cdc.png", alt: "CDC" },
-  { url: "../src/assets/sponsor/logos/logo_square/crispywill.png", alt: "ปลากรอบ" },
-  { url: "../src/assets/sponsor/logos/logo_square/lunchblock.png", alt: "Lunchblock" },
-  { url: "../src/assets/sponsor/logos/logo_square/iwear.png", alt: "IWear" },
-  { url: "../src/assets/sponsor/logos/logo_square/wherosti.png", alt: "Wherosti" },
-  { url: "../src/assets/sponsor/logos/logo_square/bitcoin_legacy.png", alt: "Bitcoin_Legacy" },  
-  { url: "../src/assets/sponsor/logos/logo_square/thai_lightning.png", alt: "Thai_Lightning" },
-  { url: "../src/assets/sponsor/logos/logo_square/catdumb.png", alt: "Catdumb" },
-  { url: "../src/assets/sponsor/logos/logo_square/spotlight.png", alt: "Spotlight" },
-  { url: "../src/assets/sponsor/logos/logo_square/siam_blockchain.png", alt: "Siam_Blockchain" },
-  { url: "../src/assets/sponsor/logos/logo_square/mgr_online.png", alt: "MGR" },
-  { url: "../src/assets/sponsor/logos/logo_square/i_bit.png", alt: "I_Bit" },
-  { url: "../src/assets/sponsor/logos/logo_square/wealthme_up.png", alt: "WealthMe_Up" },
-  { url: "../src/assets/sponsor/logos/logo_square/longtunman.png", alt: "Longtunman" },
-  { url: "../src/assets/sponsor/logos/logo_square/trader_kp.png", alt: "Trader_KP" },
-  { url: "../src/assets/sponsor/logos/logo_square/cat_money.png", alt: "Cat_Money" },
-  { url: "../src/assets/sponsor/logos/logo_square/business_tmr.png", alt: "Business_Tomorrow" },
-  { url: "../src/assets/sponsor/logos/logo_square/thairath.png", alt: "Thairath_Money" },
-  { url: "../src/assets/sponsor/logos/logo_square/ploy.png", alt: "Ploy_Storytelling" },
-  { url: "../src/assets/sponsor/logos/logo_square/lady_crypto.png", alt: "Lady_Crypto" },
-  { url: "../src/assets/sponsor/logos/logo_square/bitcoin_addict.png", alt: "Bitcoin_Addict" },
-  { url: "../src/assets/sponsor/logos/logo_square/efinance.png", alt: "Efinance" },
-
+  { url: neutronpay, alt: "Neutronpay" },
+  { url: mebit, alt: "Mebit" },
+  { url: okx, alt: "OKX" },
+  { url: beer_pot, alt: "Beer_Pot" },
+  { url: toffeecake, alt: "Toffee Cake" },
+  { url: bob, alt: "BOB" },
+  { url: chitbeer, alt: "Chitbeer" },
+  { url: bitcast, alt: "Bitcast" },
+  { url: lates, alt: "Lates" },
+  { url: trued, alt: "True Park" },
+  { url: crossvibe, alt: "Cross Vibe Hotel" },
+  { url: lumen, alt: "Lumen Bangkok Udomsuk Hotel" },
+  { url: newyorkstu, alt: "New York Stu" },
+  { url: codekids, alt: "Code_Kids" },
+  { url: seed, alt: "SE_ED" },
+  { url: digdig, alt: "DigDig" },
+  { url: oasis_coffee, alt: "Oasis_Coffee" },
+  { url: bitcoin_learning, alt: "Bitcoin_Learning" },
+  { url: sbp, alt: "SBP" },
+  { url: siambc, alt: "SiamBC" },
+  { url: shopbit, alt: "Shopbit" },
+  { url: radars_point, alt: "Radars_Point" },
+  { url: bitcoin_vn, alt: "Bitcoin_VN" },
+  { url: cdc, alt: "CDC" },
+  { url: crispywill, alt: "ปลากรอบ" },
+  { url: lunchblock, alt: "Lunchblock" },
+  { url: iwear, alt: "IWear" },
+  { url: wherosti, alt: "Wherosti" },
+  { url: bitcoin_legacy, alt: "Bitcoin_Legacy" },
+  { url: thai_lightning, alt: "Thai_Lightning" },
+  { url: catdumb, alt: "Catdumb" },
+  { url: spotlight, alt: "Spotlight" },
+  { url: siam_blockchain, alt: "Siam_Blockchain" },
+  { url: mgr_online, alt: "MGR" },
+  { url: i_bit, alt: "I_Bit" },
+  { url: wealthme_up, alt: "WealthMe_Up" },
+  { url: longtunman, alt: "Longtunman" },
+  { url: trader_kp, alt: "Trader_KP" },
+  { url: cat_money, alt: "Cat_Money" },
+  { url: business_tmr, alt: "Business_Tomorrow" },
+  { url: thairath, alt: "Thairath_Money" },
+  { url: ploy, alt: "Ploy_Storytelling" },
+  { url: lady_crypto, alt: "Lady_Crypto" },
+  { url: bitcoin_addict, alt: "Bitcoin_Addict" },
+  { url: efinance, alt: "Efinance" },
 ];
 
 export default function SecSpon() {
